ef770d07a362d1a01ec3db719e957260825ff237
shaiksajidhussain <sanjusazid0@gmail.com>
2024-01-03 18:54:15 +0530

    added animations for categories

--- script.js
+++ script.js
@@ -49,9 +49,9 @@ function shuffleArray(array) {
 }
 
 const courseData = [
-  { title: 'Python Full Course Beginners', imageSrc: 'https://i.ytimg.com/vi/7wnove7K-ZQ/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBHP0XaWCWf6WLftS6hJhmoD3aMEw', durationText: '100 Videos', watchNowLink: 'https://youtu.be/7wnove7K-ZQ?si=rAqmjf0-JcrRvJBU' },
-  { title: 'Python Full Course in English', imageSrc:'https://i.ytimg.com/vi/QXeEoD0pB3E/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDFfxTTv31qjpdalu6WVYgDswHgcg',  durationText: '110 Videos', watchNowLink: 'https://youtu.be/QXeEoD0pB3E?si=abqbDuKcGDhbEAyY' },
-  { title: 'Python Full Course in Telugu', imageSrc:'https://i.ytimg.com/an_webp/h1WdbfoHeac/mqdefault_6s.webp?du=3000&sqp=CM6s1KwG&rs=AOn4CLBw-efzq1za4Rt7jBzUJnhDz4PZZg',  durationText: '10 Hours', watchNowLink: 'https://youtu.be/h1WdbfoHeac?si=0FM4Zpd8ys8inf9C' },
+  { title: 'Python Full Course Beginners', imageSrc: '', durationText: '100 Videos', watchNowLink: 'https://youtu.be/7wnove7K-ZQ?si=rAqmjf0-JcrRvJBU' },
+  { title: 'Python Full Course in English', imageSrc:'',  durationText: '110 Videos', watchNowLink: 'https://youtu.be/QXeEoD0pB3E?si=abqbDuKcGDhbEAyY' },
+  { title: 'Python Full Course in Telugu', imageSrc:'https://i.ytimg.com/an_webp/h1WdbfoHeac/mqdefault_6s.webp?du=3000&sqp=CM6s1KwG&rs=AOn4CLBw-efzq1za4Rt7jBzUJnhDz4PZZg',  durationText: '10 Hours', watchNowLink: '' },
 ];
 
 repeatCardStructure(courseData);
